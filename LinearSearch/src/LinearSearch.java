12cec682d3e3598f78b07a2f7f374e918d0f00b3
zach <linhoway@gmail.com>
2022-02-19 23:20:59 +0800

    [Modify]Update 1. Add ArrayGenerator.java 2. Add time analysis process

--- LinearSearch/src/LinearSearch.java
+++ LinearSearch/src/LinearSearch.java
@@ -4,13 +4,10 @@
 public class LinearSearch {
 
     /**
-     *
-     * @param arr
-     * 泛型陣列內容
-     * @param target
-     * 陣列中欲查找目標
-     * @return
-     * 返回找到的元素index，若找不到則返回-1
+     * 該方法為複雜度O(n)的線性查找方法
+     * @param arr    泛型陣列內容
+     * @param target 陣列中欲查找目標
+     * @return 返回找到的元素index，若找不到則返回-1
      */
     public static <T> int search(T[] arr, T target) {
         for (int i = 0; i < arr.length; i++) {
@@ -25,29 +22,41 @@ public class LinearSearch {
         return -1;
     }
 
+    /**
+     * 該方法用於計算查找元素花費的時間，並對相同的查找執行100次，求其
+     * 總時間消耗與平均消耗。目的為觀察資料數量與時間消耗之間的關係
+     * @param numbers 生成陣列元素數量
+     * @param target 查找目標元素
+     */
+    public static <T> void timeConsume(int numbers, T target) {
+        long startTime = System.nanoTime();
+
+        for (int i = 0; i < 100; i++) {
+            if (target.getClass() == Integer.class) {
+                search(ArrayGenerator.intArrayGenerator(numbers), target);
+            } else if (target.getClass() == Double.class) {
+                search(ArrayGenerator.doubleArrayGenerator(numbers), target);
+            } else if (target.getClass() == Elements.class) {
+                search(ArrayGenerator.elementsArrayGenerator(numbers), target);
+            } else {
+                System.out.println("No such data type search method...");
+            }
+        }
+
+        long endTime = System.nanoTime();
+        double time = ((double) (endTime - startTime)) / 1000000000;
+
+        System.out.println("For numbers : " + numbers + ", runs 100 times, total consume : " + time + "s");
+        System.out.println("Average time consume : " + time / 100);
+    }
+
     public static void main(String[] args) {
-        /* 整數類型線性查找 */
-        Integer[] arr = {12, 65, 98, 4, 77, 14};
-        System.out.println("Index : " + search(arr, 4));
-        System.out.println("Index : " + search(arr, 666));
-
-        System.out.println("=======================================");
-
-        /* 浮點類型線性查找 */
-        Double[] arr2 = {12.3, 78.6, 9.9, 45.3, 12.6, 22.2};
-        System.out.println("Index : " + search(arr2, 9.9));
-        System.out.println("Index : " + search(arr2, 22.2));
-
-        System.out.println("=======================================");
-
-        /* 自定義類線性查找 */
-        Elements[] arr3 = new Elements[3];
-        Elements target = new Elements("S003", 3);
-        arr3[0] = new Elements("s001", 1);
-        arr3[1] = new Elements("s002", 2);
-        arr3[2] = new Elements("s003", 3);
-
-        System.out.println("Index : " + search(arr3, target));
+        int[] times = {
+            100000, 1000000
+        } ;
+        for (int i : times) {
+            timeConsume(i, 10);
+        }
     }
 
 }
