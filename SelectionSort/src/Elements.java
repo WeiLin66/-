6bbbd3ffca17e41e47ebb1cceab35e00225b4490
zach <linhoway@gmail.com>
2022-02-20 23:44:41 +0800

    [Modify]Update 1. Add Reverse Selection Sort 2. Add SortingHelper.java

---
+++ SelectionSort/src/Elements.java
@@ -0,0 +1,75 @@
+/**
+ * 自定義Elements類
+ */
+public class Elements implements Comparable<Elements> {
+    private String id;
+    private int number;
+
+    public Elements(String id, int number) {
+        setId(id);
+        setNumber(number);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    /**
+     * 重寫的equals方法，用於比對成員屬性是否相同
+     *
+     * @param obj 傳入比較對象
+     * @return 若兩對象屬性相同則返回true，否則返回false。屬性id比較無關大小寫
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null) {
+            System.out.println("class is null...");
+            return false;
+        }
+
+        if (obj.getClass() != Elements.class) {
+            System.out.println("Wrong class type...");
+            return false;
+        }
+
+        return (getId().equalsIgnoreCase(((Elements) obj).getId()) && getNumber() == ((Elements) obj).getNumber());
+
+    }
+
+    /**
+     * 重寫的toString方法，用於打印類資訊
+     *
+     * @return Elements類資訊
+     */
+    @Override
+    public String toString() {
+        return "Elements[ ID : " + getId() + ", Number : " + getNumber() + " ]";
+    }
+
+    /**
+     * 實現Comparable接口中的compareTo()方法
+     *
+     * @param o Elements類型對象
+     * @return 正數如果當前類的number較大；負數傳入的對象number較大；0兩者的number相等
+     */
+    @Override
+    public int compareTo(Elements o) {
+        return this.getNumber() - o.getNumber();
+    }
+}
--- SelectionSort/src/SelectionSort.java
+++ SelectionSort/src/SelectionSort.java
@@ -1,37 +1,58 @@
+/**
+ * 選擇排序實現類
+ */
 public class SelectionSort {
-    public static <T extends Comparable> void selectionSort(T[] arr) {
-        int head = 0, find = -1;
-        T small, temp;
-
-        while (head < arr.length - 1) {
-            small = arr[head];
-            for (int i = head; i < arr.length - 1; i++) {
-                try {
-                    if (small.compareTo(arr[i + 1]) > 0) {
-                        find = i + 1;
-                        small = arr[i + 1];
-                    }
-                } catch (ArrayIndexOutOfBoundsException e) {
-                    e.printStackTrace();
+    /**
+     * 選擇排序，[0...i)為已處理；[i...n)為未處理
+     *
+     * @param arr 對象陣列
+     */
+    public static <T extends Comparable<T>> void sort(T[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int find = i;
+            for (int j = i; j < arr.length; j++) {
+                if (arr[j].compareTo(arr[find]) < 0) {
+                    find = j;
                 }
             }
-
-            if (find != -1) {
-                temp = arr[find];
-                arr[find] = arr[head];
-                arr[head] = temp;
-                find = -1;
-            }
-            head++;
-        }
-
-        for (T i : arr) {
-            System.out.print(i + " ");
+            swap(arr, i, find);
         }
     }
 
+    /**
+     * 選擇排序，[i...n)為已處理；[0...i)為未處理
+     *
+     * @param arr 對象陣列
+     */
+    public static <T extends Comparable<T>> void reverseSort(T[] arr) {
+        for (int i = arr.length - 1; i >= 0; i--) {
+            int find = i;
+            for (int j = i; j >= 0; j--) {
+                if (arr[find].compareTo(arr[j]) < 0) {
+                    find = j;
+                }
+            }
+            swap(arr, i, find);
+        }
+    }
+
+    /**
+     * 將陣列中index為head、find兩元素交換
+     *
+     * @param arr  對象陣列
+     * @param head 元素A index
+     * @param find 元素B index
+     */
+    public static <T> void swap(T[] arr, int head, int find) {
+        T temp = arr[find];
+        arr[find] = arr[head];
+        arr[head] = temp;
+    }
+
     public static void main(String[] args) {
-        Integer[] integers = ArrayGenerator.intArrayGenerator(10);
-        selectionSort(integers);
+        int[] dataSize = {10000, 100000};
+        for (int i : dataSize) {
+            SortingHelper.sortTest("Reverse Selection Sort", ArrayGenerator.intArrayGenerator(i, i));
+        }
     }
 }
