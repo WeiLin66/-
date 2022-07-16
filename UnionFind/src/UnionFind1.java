946ca9c7aeda445e822dc70ca841453879898faa
Hau-Wei <linhoway@gmail.com>
2022-07-16 23:19:21 +0800

    [Update]Union 1. UnionFind1 2. UnionFind2

---
+++ UnionFind/src/UnionFind1.java
@@ -0,0 +1,45 @@
+public class UnionFind1 implements UF {
+
+    private int[] id;
+
+    public UnionFind1(int size) {
+        id = new int[size];
+
+        for (int i = 0; i < id.length; i++) {
+            id[i] = i;
+        }
+    }
+
+    @Override
+    public int getSize() {
+        return id.length;
+    }
+
+    @Override
+    public boolean isConnected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    @Override
+    public void unionElements(int p, int q) {
+        int pID = find(p);
+        int qID = find(q);
+
+        if (pID == qID){
+            return;
+        }
+
+        for (int i = 0; i < getSize(); i++) {
+            if (id[i] == qID) {
+                id[i] = pID;
+            }
+        }
+    }
+
+    private int find(int p) {
+        if (p < 0 || p >= id.length) {
+            throw new IllegalArgumentException("invalid index!");
+        }
+        return id[p];
+    }
+}
